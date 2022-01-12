a21bb397efba1e3aa28eaa921638d8054bc9503c
mick <mike.120t@gmail.com>
2022-01-12 02:19:52 +0100

    FnG simulator created and running

--- run_backtest.py
+++ run_backtest.py
@@ -6,35 +6,50 @@ import simulators as sim
 
 # set parameters
 
+#FnG data
+fng_json_path = r'data_crypto/FnG_to_2022-01-11.json'
+
 # ADA/USDT
-pair = 'ADA-USD'
-start_date = '2021-03-18'
-init_buy = 1.5
-profit_rate = 0.1   # 10%
-quantum = 100       # $100
-init_cash = 1500    # $1000
+# pair = 'ADA-USD'
+# start_date = '2021-03-18'
+# init_buy = 1.5
+# profit_rate = 0.1   # 10%
+# quantum = 100       # $100
+# init_cash = 1500    # $1000
 
 # ETH/BTC
-# pair = 'ETH-BTC'
-# start_date = '2021-01-01'
+pair = 'ETH-BTC'
+start_date = '2021-01-01'
+init_buy = 0.025  # limit price for first buy
+profit_rate = 0.02       # wanted profit at each trade
+quantum = 0.00072
+init_cash = 0.022
+
+# ETH/BTC
+pair = 'BTC-USD'
+start_date = '2021-01-01'
 # init_buy = 0.025  # limit price for first buy
 # profit_rate = 0.02       # wanted profit at each trade
-# quantum = 0.00072
-# init_cash = 0.022
+quantum = 100
+init_cash = 1000
 
 # load data
 with open(f'data_crypto/{pair}_{start_date}.pkl', 'rb') as file:
     data_df = pkl.load(file)
 
 # init wallet
-wallet = WalletIdca(init_cash_base=init_cash)
-wallet.init_buy_order(quantum, 1.5)
-
+# wallet = WalletIdca(init_cash_base=init_cash)
 # do the simulation
-sim.idca_1st_approach(data_df, wallet, quantum, profit_rate, upbuy=False)
+# wallet.init_buy_order(quantum, 1.5)
+# sim.idca_1st_approach(data_df, wallet, quantum, profit_rate, upbuy=False)
 
-print('Buy order', wallet.buy_order)
-print('Sell orders', wallet.sell_orders)
+# init wallet
+wallet = Wallet(init_cash)
+#FnG
+sim.fng(fng_json_path, data_df, wallet, quantum)
+
+# print('Buy order', wallet.buy_order)
+# print('Sell orders', wallet.sell_orders)
 print(f'\nFinal rate is\t\t{data_df.iloc[-1]["close"]:3.3f}')
 print(f'Profit:\t\t\t\t{wallet.balance_base(data_df.iloc[-1]["close"]) / init_cash * 100 - 100:3.0f} %')
 print(f'Market performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
--- simulators.py
+++ simulators.py
@@ -1,7 +1,10 @@
 import Wallets
+import json
+import pandas as pd
+from datetime import datetime
 
 
-def idca_1st_approach(data, wallet: Wallets.WalletIdca, quantum, profit_rate, upbuy=False):
+def idca_1st_approach(data: pd.DataFrame, wallet: Wallets.WalletIdca, quantum, profit_rate, upbuy=False):
     """
     Buys on drops, sells on ups.
     :param data: exchange data
@@ -39,3 +42,48 @@ def idca_1st_approach(data, wallet: Wallets.WalletIdca, quantum, profit_rate, up
                       f"balance = {wallet.balance_base(wallet.buy_order[1]):5.7f} [base]")
 
     print('Simulation FINISHED')
+
+
+def fng(fng_json_path: str, data: pd.DataFrame, wallet: Wallets.Wallet, quantum, buy_threshold=30, sell_threshold=60):
+    """
+    Strategy based on Fear & Greed index from alternative.me
+    :param fng_json_path: path to json with historical FnG data
+    :param data: exchange data
+    :param wallet: wallet object
+    :param quantum: trading quantum in base currency
+    :param buy_threshold: buy if fng is more than
+    :param sell_threshold: sell if fng is less than
+    """
+    fng_df = json.load(open(fng_json_path))
+    fng_df = pd.DataFrame(fng_df['data'])
+    fng_df['value'] = fng_df['value'].astype(int)
+    fng_df['timestamp'] = fng_df['timestamp'].astype(int)
+    fng_df['timestamp'] = fng_df['timestamp'].apply(lambda x: datetime.utcfromtimestamp(x).strftime('%Y-%m-%d'))
+    fng_df = fng_df.set_index(pd.to_datetime(fng_df['timestamp']))
+    fng_df = fng_df.drop(['value_classification', 'time_until_update', 'timestamp'], axis=1)
+    fng_df = fng_df.sort_index()
+
+    # reduce the dataset temporarily
+    fng_df = fng_df[(fng_df.index < '2022-01-01') & (fng_df.index > '2021-01-01')]
+
+    buy = True  # wanna buy flag
+
+    for index, row in fng_df.iterrows():
+
+        # print(index, row.value)
+
+        if buy and row.value <= buy_threshold:
+            timestamp = (index + pd.Timedelta(hours=10)).strftime('%Y-%m-%d-%H-%M')
+            rate = data.loc[timestamp]['open']
+            wallet.buy(quantum, rate)
+            buy = False
+
+            print(f'{timestamp}: BUY   @ {rate:.7f}, {wallet.base:05.7f} [base], {wallet.quote:05.7f} [quote]')
+
+        elif not buy and row.value >= sell_threshold:
+            timestamp = (index + pd.Timedelta(hours=10)).strftime('%Y-%m-%d-%H-%M')
+            rate = data.loc[timestamp]['open']
+            wallet.sell(wallet.history[-1][2] / wallet.history[-1][1] * (1 - wallet.fee) - 0.0001 / rate, rate)
+            buy = True
+
+            print(f'{timestamp}: SELL @ {rate:.7f}, {wallet.base:05.7f} [base], {wallet.quote:05.7f} [quote]')
