dc7dbc658cd5278a6d3c8776f7dc7ae553b45dce
mick <mike.120t@gmail.com>
2022-04-28 21:54:29 +0200

    two functions added: running grid ones and running grid in a loop for parameter ranges

--- runners/run_a_grid.py
+++ runners/run_a_grid.py
@@ -7,36 +7,74 @@ import utils
 from pathlib import Path
 import simulators as sim
 
-# set parameters
-start_date = '2021-11-01'
-end_date = '2021-11-30'
 
-data_folder = 'data_crypto'
+def run_grid_loop(pair, start_date, end_date,
+                  profit_rate_list, n_steps_list, sell_under_top_list, buy_under_top_list):
+    """
+    Run grid simulation for multiple parameter values in a loop.
+    :return:
+    """
+    quantum = 100
+    init_quote = 1000
+    data_folder = 'data_crypto'
+
+    # load historical data
+    data_df = utils.load_crypto_data(pair, start_date, end_date, data_folder)
+
+    init_buy_rate = data_df["open"][0]
+
+    for profit_rate in profit_rate_list:
+        for n_steps in n_steps_list:
+            for sell_under_top in sell_under_top_list:
+                for buy_under_top in buy_under_top_list:
+                    prof, _ = run_grid_once(data_df, init_quote, quantum, init_buy_rate,
+                                            profit_rate, n_steps, sell_under_top, buy_under_top,
+                                            verbose=False)
+                    print(f'profit_rate: {profit_rate:.2f} '
+                          f'n_steps: {n_steps:>2} '
+                          f'sell_under_top: {sell_under_top:.2f} '
+                          f'buy_under_top: {buy_under_top:.2f} '
+                          f'--> profit: {prof * 100:3.0f} %')
+
+    print(f'\nMarket performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
 
 
-# BTC/USD
-pair = 'BTC-USD'
-profit_rate = 0.02       # wanted profit at each trade
-quantum = 100
-init_quote = 1000
-# init_base = 0
-# init_buy = 41000  # limit price for first buy
-n_steps = 10
-sell_under_top = 0.03
-buy_under_top = 0.12
+def run_grid_once(data_df, init_vol_quote, quantum, init_buy_rate,
+                  profit_rate, n_steps, sell_under_top, buy_under_top, verbose=True):
+    """
+    Runs a single grid simulation.
+    :return: wallet,
+    """
+    # init wallet
+    wallet = Wallet(init_cash_quote=init_vol_quote)
+    # do the simulation
+    # wallet.init_buy_order(quantum, init_buy)
+    sim.a_grid(data_df, wallet, quantum, init_buy_rate,
+               profit_rate, n_steps, sell_under_top, buy_under_top,
+               verbose=verbose)
 
-# load historical data
-data_df = utils.load_crypto_data(pair, start_date, end_date, data_folder)
+    profit = wallet.balance_quote(data_df.iloc[-1]["close"]) / init_vol_quote - 1
 
-init_buy = data_df["open"][0]
+    # print(f'\nFinal rate is\t\t{data_df.iloc[-1]["close"]:3.3f}')
+    # print(f'Profit:\t\t\t\t{wallet.balance_quote(data_df.iloc[-1]["close"]) / init_vol_quote * 100 - 100:3.0f} %')
+    # print(f'Market performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
 
-# ---- advanced grid
-# init wallet
-wallet = Wallet(init_cash_quote=init_quote)
-# do the simulation
-# wallet.init_buy_order(quantum, init_buy)
-sim.a_grid(data_df, wallet, quantum, profit_rate, init_buy, n_steps, sell_under_top, buy_under_top)
+    return profit, wallet
 
-print(f'\nFinal rate is\t\t{data_df.iloc[-1]["close"]:3.3f}')
-print(f'Profit:\t\t\t\t{wallet.balance_quote(data_df.iloc[-1]["close"]) / init_quote * 100 - 100:3.0f} %')
-print(f'Market performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
+
+if __name__ == '__main__':
+
+    # set parameters
+    pair = 'ETH-BTC'
+    start_date = '2022-01-01'
+    end_date = '2022-01-31'
+    # end_date = '2022-04-27'
+
+    profit_rates = [0.02, 0.03, 0.04]       # wanted profit at each trade
+    n_stepss = [10, 8, 6]
+    sell_under_tops = [0.03]
+    buy_under_tops = [0.12]
+
+    run_grid_loop(pair, start_date, end_date,
+                  profit_rates, n_stepss, sell_under_tops, buy_under_tops)
+
