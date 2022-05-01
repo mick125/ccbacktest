bf8bcb85230db94cb6fe06d411f1f61d829a0dac
mick <mike.120t@gmail.com>
2022-05-01 22:03:03 +0200

    run_grid_loop uses multiprocessing now

--- runners/run_a_grid.py
+++ runners/run_a_grid.py
@@ -2,14 +2,17 @@ import os
 import pandas as pd
 import pickle as pkl
 
-from Wallets import *
 import utils
+from joblib import Parallel, delayed
 from pathlib import Path
+
+from Wallets import *
 import simulators as sim
 
 
 def run_grid_loop(pair, start_date, end_date,
-                  profit_rate_list, n_steps_list, sell_under_top_list, buy_under_top_list):
+                  profit_rate_list, n_steps_list, sell_under_top_list, buy_under_top_list,
+                  n_cpu=8):
     """
     Run grid simulation for multiple parameter values in a loop.
     :return:
@@ -23,20 +26,21 @@ def run_grid_loop(pair, start_date, end_date,
 
     init_buy_rate = data_df["open"][0]
 
-    for profit_rate in profit_rate_list:
-        for n_steps in n_steps_list:
-            for sell_under_top in sell_under_top_list:
-                for buy_under_top in buy_under_top_list:
-                    prof, _ = run_grid_once(data_df, init_quote, quantum, init_buy_rate,
-                                            profit_rate, n_steps, sell_under_top, buy_under_top,
-                                            verbose=False)
-                    print(f'profit_rate: {profit_rate:.2f} '
-                          f'n_steps: {n_steps:>2} '
-                          f'sell_under_top: {sell_under_top:.2f} '
-                          f'buy_under_top: {buy_under_top:.2f} '
-                          f'--> profit: {prof * 100:3.0f} %')
+    # run grid bots for all parameter combinations in parallel
+    res = Parallel(n_jobs=n_cpu)(delayed(run_grid_once)
+                                 (data_df, init_quote, quantum, init_buy_rate,
+                                  profit_rate, n_steps, sell_under_top, buy_under_top,
+                                  verbose=False)
+                                 for profit_rate in profit_rate_list
+                                 for n_steps in n_steps_list
+                                 for sell_under_top in sell_under_top_list
+                                 for buy_under_top in buy_under_top_list)
 
-    print(f'\nMarket performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
+    market_performance = data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] - 1
+
+    print(f'\nMarket performance:\t{market_performance:3.0f} %')
+
+    return res, market_performance
 
 
 def run_grid_once(data_df, init_vol_quote, quantum, init_buy_rate,
@@ -55,20 +59,22 @@ def run_grid_once(data_df, init_vol_quote, quantum, init_buy_rate,
 
     profit = wallet.balance_quote(data_df.iloc[-1]["close"]) / init_vol_quote - 1
 
-    # print(f'\nFinal rate is\t\t{data_df.iloc[-1]["close"]:3.3f}')
-    # print(f'Profit:\t\t\t\t{wallet.balance_quote(data_df.iloc[-1]["close"]) / init_vol_quote * 100 - 100:3.0f} %')
-    # print(f'Market performance:\t{data_df.iloc[-1]["close"] / data_df.iloc[0]["open"] * 100 - 100:3.0f} %')
+    print(f'profit_rate: {profit_rate:.2f}, '
+          f'n_steps: {n_steps:>2}, '
+          f'sell_under_top: {sell_under_top:.2f}, '
+          f'buy_under_top: {buy_under_top:.2f}, '
+          f'--> profit: {profit * 100:3.0f} %')
 
     return profit, wallet
 
 
 if __name__ == '__main__':
-
     # set parameters
     pair = 'ETH-BTC'
     start_date = '2022-01-01'
     end_date = '2022-01-31'
     # end_date = '2022-04-27'
+    n_cpu = 8
 
     profit_rates = [0.02, 0.03, 0.04]       # wanted profit at each trade
     n_stepss = [10, 8, 6]
@@ -76,5 +82,5 @@ if __name__ == '__main__':
     buy_under_tops = [0.12]
 
     run_grid_loop(pair, start_date, end_date,
-                  profit_rates, n_stepss, sell_under_tops, buy_under_tops)
-
+                  profit_rates, n_stepss, sell_under_tops, buy_under_tops,
+                  n_cpu=n_cpu)
