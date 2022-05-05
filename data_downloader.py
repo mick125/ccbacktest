1fdb23f3d722df1d81ea082f836b18533b01a4ac
mick <mike.120t@gmail.com>
2022-05-05 23:51:14 +0200

    bug in data downloader fixed

--- data_downloader.py
+++ data_downloader.py
@@ -22,12 +22,13 @@ def retrieve_and_save_hist_data(pair: str, t_start: pd.Timestamp, t_end: pd.Time
         f'data_raw/{pair}_{t_start.strftime(date_format_file)}_to_{t_end.strftime(date_format_file)}.parquet')
 
 
-def retrieve_and_save_hist_data_parallel(pair: str, start_datetime: str, end_datetime: str) -> None:
+def retrieve_and_save_hist_data_parallel(pair: str, start_datetime: str, end_datetime: str, chunk_size='6M') -> None:
     """
     Retrieves historical crypto data from coin quote and saves them year-wise into parquet files. In parallel!
     :param pair: Currency pair
     :param start_datetime: Get data from exchange from this date (format %Y-%m-%d-%H-%M)
     :param end_datetime: Get data from exchange to this date (format %Y-%m-%d-%H-%M)
+    :param chunk_size: Time length of a data chunk to be downloaded in one thread
     """
     start_datetime = pd.to_datetime(start_datetime, format=date_format)
     end_datetime = pd.to_datetime(end_datetime, format=date_format)
@@ -35,10 +36,11 @@ def retrieve_and_save_hist_data_parallel(pair: str, start_datetime: str, end_dat
     start_times = list()
     # end_times = list()
 
-    # TODO not working properly for periods shorter than 6M
-    end_times = pd.date_range(start_datetime, end_datetime, freq='6M').tolist()
-
+    end_times = pd.date_range(start_datetime, end_datetime, freq=chunk_size).tolist()
     end_times = [time + pd.Timedelta(hours=23, minutes=59) for time in end_times]
+    if end_datetime != end_times[-1]:
+        end_times.append(end_datetime)
+
     start_times.append(pd.to_datetime(start_datetime, format=date_format))
     start_times.extend([time + pd.Timedelta(minutes=1) for time in end_times])
     start_times.pop()
@@ -62,10 +64,11 @@ if __name__ == '__main__':
     # pair = 'ETH-USD'
     # pair = 'LTC-USD'
     pair = 'ETH-BTC'
-    start_datetime = '2022-01-01-00-00'
-    end_datetime = '2022-04-27-23-59'
+    start_datetime = '2016-06-01-00-00'
+    end_datetime = '2022-04-30-23-59'
+    chunk_length = '3M'
     # retrieve_and_save_hist_data(pair,
     #                             pd.to_datetime(start_datetime, format=date_format),
     #                             pd.to_datetime(end_datetime, format=date_format))
 
-    retrieve_and_save_hist_data_parallel(pair, start_datetime, end_datetime)
+    retrieve_and_save_hist_data_parallel(pair, start_datetime, end_datetime, chunk_size=chunk_length)
